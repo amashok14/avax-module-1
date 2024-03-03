edce54b28b6f67d8543549d5f89a57e5e48f5ee5
amashok14 <149903401+amashok14@users.noreply.github.com>
2024-03-04 00:17:52 +0530

    update the mod

---
+++ mod.sol
@@ -0,0 +1,24 @@
+# BankSystem Contract
+
+## Description
+The BankSystem contract is a simple smart contract written in Solidity. It allows users to deposit funds into the contract and withdraw them if they are the owner.
+
+## Features
+- Deposit: Users can deposit funds into the contract.
+- Withdraw: Only the owner can withdraw funds from the contract.
+
+## Requirements
+- Solidity compiler ^0.8.0
+
+## Usage
+1. Deploy the contract to an Ethereum-compatible blockchain.
+2. Interact with the contract using a web3 provider or Ethereum wallet.
+
+## Contract Address
+[Contract Address will be here]
+
+## ABI
+[ABI will be here]
+
+## License
+This project is licensed under the UNLICENSED License - see the [LICENSE](LICENSE) file for details.
